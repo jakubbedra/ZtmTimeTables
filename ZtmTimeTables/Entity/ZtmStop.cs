a8c79d0ca958c69f70995c9342509ac6022261b0
jakubbedra <jakubbedra@wp.pl>
2022-11-24 01:33:55 +0100

    a

--- ZtmTimeTables/Entity/ZtmStop.cs
+++ ZtmTimeTables/Entity/ZtmStop.cs
@@ -2,7 +2,25 @@ namespace ZtmTimeTables.Entity;
 
 public class ZtmStop
 {
-    public int Id { get; set; }
-    public string Location { get; set; }
-    public virtual ICollection<ZtmVehicleArrival> VehicleArrivals { get; set; }
+    public int? StopId { get; set; }
+    public string? StopCode { get; set; }
+    public string? StopName { get; set; }
+    public string? StopShortName { get; set; }
+    public string? StopDesc { get; set; }
+    public string? SubName { get; set; }
+    public int? ZoneId { get; set; }
+    public string? ZoneName { get; set; }
+    public int? Virtual { get; set; }
+    public int? Nonpassenger { get; set; }
+    public int? Depot { get; set; }
+    public int? TicketZoneBorder { get; set; }
+    public int? OnDemand { get; set; }
+    public string? ActivationDate { get; set; }
+    public double? StopLat { get; set; }
+    public double? StopLon { get; set; }
+    public string? StopUrl { get; set; }
+    public string? LocationType { get; set; }
+    public string? ParentStation { get; set; }
+    public string? StopTimezone { get; set; }
+    public string? WheelchairBoarding { get; set; }
 }
